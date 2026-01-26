1ab82fa33f6068ec7d6c86663a6743bad703b44c
Pavel Vandyshev <pavell_f@mail.ru>
2026-01-26 15:56:36 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import { HistoryData } from './components/HistoryData/HistoryData';
+
+function App() {
+  return (
+    <div className="page">
+
+<div className='container'>
+
+<HistoryData/>
+
+</div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HistoryData/HistoryData.tsx
@@ -0,0 +1,4 @@
+import styles from "./HistoryData.module.scss";
+export const HistoryData = () => {
+  return <div className={styles.historyBlock}>HistoryData</div>;
+};
